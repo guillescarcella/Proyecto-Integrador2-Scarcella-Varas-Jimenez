ec9c479ade8184e6ef8092cac768495ddfafef54
MariaVaras <mariavarascastex@icloud.com>
2023-04-07 11:28:54 -0300

    trabajando un data

--- ProyectoIntegrador2/data/data.js
+++ ProyectoIntegrador2/data/data.js
@@ -1,22 +1,86 @@
 const data= {
     usuario: {
         nombre: 'g',
-        contrasena:'gh'
+        contrasena:'gh',
+        mail: 'mariavaras@hotmail.com',
+        fechaDeNacimiento: '10/08/2002',
+        DNI: '44366344',
+        fotoDePerfil: '/images/users/default-image.png'
     },
     productos: [
         {
-            imagen: 'ruta a imagen',
-            nombre:'',
-            descripcion: '',
-            fecha: 20/01/2023
-        }
+            imagen: '/images/products/labial.jpg',
+            nombre:'Labial',
+            descripcion: 'Super brilloso!',
+            fecha: '20/01/2023'
+        },
+        {
+            imagen: '/images/products/rimel.jpg',
+            nombre:'Rimel',
+            descripcion: 'Nueva formula',
+            fecha: '20/02/2023'
+        },
+        {
+            imagen: '/images/products/lapiz.jpg',
+            nombre:'Lapiz',
+            descripcion: 'Delinea tus labios!',
+            fecha: '10/03/2023'
+        },
+        {
+            imagen: '/images/products/base.jpg',
+            nombre:'Base',
+            descripcion: 'Ponele color a tu cara',
+            fecha: '10/04/2023'
+        },
+        {
+            imagen: '/images/products/broncer.jpg',
+            nombre:'Broncer',
+            descripcion: 'Defini tus pomulos',
+            fecha: '10/05/2023'
+        },
+        {
+            imagen: '/images/products/Iluminador.jpg',
+            nombre:'Iluminador',
+            descripcion: 'Iluniate',
+            fecha: '10/06/2023'
+        },
+        {
+            imagen: '/images/products/brochas.jpg',
+            nombre:'Brocha',
+            descripcion: 'Nuevo set de brochas',
+            fecha: '10/07/2023'
+        },
+        {
+            imagen: '/images/products/paleta.jpg',
+            nombre:'Paleta',
+            descripcion: 'Muchos colores para tus ojos',
+            fecha: '10/08/2023'
+        },
+        {
+            imagen: '/images/products/pintalabios.jpg',
+            nombre:'Pinta labios',
+            descripcion: 'Nuevo rojo intenso',
+            fecha: '10/09/2023'
+        },
+        {
+            imagen: '/images/products/Peine de cejas.jpg',
+            nombre:'Peine de cejas',
+            descripcion: 'Dale forma a tus cejas',
+            fecha: '10/10/2023'
+        },
     ],
     
-    comentarios:{
+    comentarios:[{
+        usuario: 'mery',
+        comentario:'Hola sos lo mas',
+        imagen:'/images/users/default-image.png'
+    },
+    {
         usuario: '',
         comentario:'',
         imagen:''
-    }
+    },
+    ]
 }
 
 module.exports=data
