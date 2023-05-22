dee66519136ba72fae8ea8575adc2855a2161b57
guillescarcella <scarcellag@udesa.edu.ar>
2023-05-22 12:42:11 -0300

    modelos

---
+++ ProyectoIntegrador2/database/models/Producto.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, dataTypes ) {
+    let alias = "Producto";
+    let cols = {
+        id: {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.INTEGER
+        },
+        name: {
+            type : dataTypes.STRING
+        }
+  	}
+
+    let config = {
+        tableName: "movies",
+        timestamps: false,
+        underscored: true
+    }
+    
+    let Movies = sequelize.define(alias, cols, config);
+    return Movies;
+}
