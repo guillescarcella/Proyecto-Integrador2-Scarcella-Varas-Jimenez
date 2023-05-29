4a652ac44bf1370fe08507ac675b862a8e5f857c
delfinajimenezherrera <macbookpro@MacBooks-MacBook-Pro.local>
2023-05-29 11:35:23 -0300

    cambios

--- ProyectoIntegrador2/controllers/productController.js
+++ ProyectoIntegrador2/controllers/productController.js
@@ -1,7 +1,12 @@
 const data= require('../data/data')
+const db = require('../database/models')
+const Producto = db.Producto
 const productController = {
 
     product: function(req, res) {
+      Producto.findAll().then(function (data) {
+        console.log(data);
+      })
         return res.render('product', {product: data.productos[0], comentarios: data.comentarios});
       },
     productAdd: function(req, res) {
